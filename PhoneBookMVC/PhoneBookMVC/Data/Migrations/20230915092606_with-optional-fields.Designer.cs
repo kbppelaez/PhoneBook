f4d1e962bcc934ca7f05d6655ec359f33d347bde
kbppelaez <kppelaez@up.edu.ph>
2023-09-15 19:16:58 +0800

    Updated database about optional fields

--- PhoneBookMVC/PhoneBookMVC/Data/Migrations/20230915092606_with-optional-fields.Designer.cs
+++ PhoneBookMVC/PhoneBookMVC/Data/Migrations/20230915092606_with-optional-fields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PhoneBookMVC.Data;
 
@@ -11,9 +12,10 @@ using PhoneBookMVC.Data;
 namespace PhoneBookMVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230915092606_with-optional-fields")]
+    partial class withoptionalfields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
