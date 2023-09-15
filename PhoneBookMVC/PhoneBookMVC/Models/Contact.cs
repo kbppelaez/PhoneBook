e2ff6321dbfd2e664fa5765c0f6524437e057275
kbppelaez <kppelaez@up.edu.ph>
2023-09-15 17:24:04 +0800

    Made some attributes optional

--- PhoneBookMVC/PhoneBookMVC/Models/Contact.cs
+++ PhoneBookMVC/PhoneBookMVC/Models/Contact.cs
@@ -13,23 +13,30 @@ namespace PhoneBookMVC.Models
 
         [StringLength(50)]
         [Display(Name = "Last Name")]
-        public string LastName { get; set; } = string.Empty;
+        public string? LastName { get; set; }
 
         [Display(Name = "Email Address")]
-        [RegularExpression(@"[_a-zA-Z0-9.]+@([a-zA-Z]{2,}[.])+[a-zA-Z]{2,}")]
-        public string Email { get; set; } = string.Empty;
+        [RegularExpression(
+            @"[_a-zA-Z0-9.]+@([a-zA-Z]{2,}[.])+[a-zA-Z]{2,}",
+            ErrorMessage = "Please input a valid email address.")]
+        public string? Email { get; set; }
 
         [Display(Name = "Phone Number")]
-        public string PhoneNumber { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
 
         [Display(Name = "Notes")]
-        public string Notes { get; set; } = string.Empty;
+        public string? Notes { get; set; }
 
+        [Display(Name = "Name")]
         public string FullName
         {
             get
             {
-                return FirstName + " " + LastName;
+                if (LastName != null)
+                {
+                    return FirstName + " " + LastName;
+                }
+                else return FirstName;
             }
         }
     }
