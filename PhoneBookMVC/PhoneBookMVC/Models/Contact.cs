7a4c58de84c0c93badfbeaf286db5317a0a1aeae
kbppelaez <kppelaez@up.edu.ph>
2023-09-15 16:42:13 +0800

    Created Data Model for a Contact

--- PhoneBookMVC/PhoneBookMVC/Models/Contact.cs
+++ PhoneBookMVC/PhoneBookMVC/Models/Contact.cs
@@ -1,6 +1,36 @@
-﻿namespace PhoneBookMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBookMVC.Models
 {
     public class Contact
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Display(Name = "Email Address")]
+        [RegularExpression(@"[_a-zA-Z0-9.]+@([a-zA-Z]{2,}[.])+[a-zA-Z]{2,}")]
+        public string Email { get; set; } = string.Empty;
+
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Display(Name = "Notes")]
+        public string Notes { get; set; } = string.Empty;
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
     }
 }
