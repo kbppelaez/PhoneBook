faf7944f533f2db62efb4a5e778e932225efbc2c
kbppelaez <kppelaez@up.edu.ph>
2023-09-15 19:17:23 +0800

    Made optional attributes nullable

--- PhoneBookMVC/PhoneBookMVC/Models/Contact.cs
+++ PhoneBookMVC/PhoneBookMVC/Models/Contact.cs
@@ -8,7 +8,7 @@ namespace PhoneBookMVC.Models
 
         [Required]
         [StringLength(50, MinimumLength = 1)]
-        [Display(Name = "First Name")]
+        [Display(Name = "First Name (*)")]
         public string FirstName { get; set; } = string.Empty;
 
         [StringLength(50)]
@@ -17,8 +17,8 @@ namespace PhoneBookMVC.Models
 
         [Display(Name = "Email Address")]
         [RegularExpression(
-            @"[_a-zA-Z0-9.]+@([a-zA-Z]{2,}[.])+[a-zA-Z]{2,}",
-            ErrorMessage = "Please input a valid email address.")]
+            @"[_a-zA-Z0-9.]+@([a-zA-Z]{2,}[.])+[a-zA-Z]{2,}",       //alphanumeric and _ . for username, with at least one subdomain and at least one TLD
+            ErrorMessage = "Please input a valid email address.")]  //      at least 2 letters per domain
         public string? Email { get; set; }
 
         [Display(Name = "Phone Number")]
