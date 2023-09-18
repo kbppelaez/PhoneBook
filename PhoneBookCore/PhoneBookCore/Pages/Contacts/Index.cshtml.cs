3f16c41dc4af0ef52f69ae545d9abb41056f878f
kbppelaez <kppelaez@up.edu.ph>
2023-09-18 16:32:21 +0800

    Added connection to db and putting each contact to a List

--- PhoneBookCore/PhoneBookCore/Pages/Contacts/Index.cshtml.cs
+++ PhoneBookCore/PhoneBookCore/Pages/Contacts/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace PhoneBookCore.Pages.Contacts
 {
@@ -9,13 +10,45 @@ namespace PhoneBookCore.Pages.Contacts
 
         public void OnGet()
         {
+            try
+            {
+                String connectionString = "Data Source=.\\sqlexpress;Initial Catalog=Phonebook;User ID=sa;Password=***********";
 
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    String query = "SELECT * FROM Contact;";
+                    using(SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ContactInfo contact = new ContactInfo();
+                                contact.Id = reader.GetInt32(0);
+                                contact.FirstName = reader.GetString(1);
+                                contact.LastName = reader.GetString(2);
+                                contact.EmailAdd = reader.GetString(3);
+                                contact.PhoneNumber = reader.GetString(4);
+                                contact.Notes = reader.GetString(5);
+
+                                contacts.Add(contact);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
         }
     }
 
     public class ContactInfo
     {
-        public string Id;
+        public int Id;
         public string FirstName;
         public string LastName;
         public string EmailAdd;
