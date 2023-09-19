10354b43a28abd860b2bda04dd9d8a5fa2060332
kbppelaez <kppelaez@up.edu.ph>
2023-09-19 10:38:47 +0800

    Generated Razor Page for Searching

---
+++ PhoneBookCore/PhoneBookCore/Pages/Contacts/SearchView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhoneBookCore.Pages.Contacts
+{
+    public class SearchViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
