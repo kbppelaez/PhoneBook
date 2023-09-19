9128db21fd140f958fe2f0f86232cd686a78f4b3
kbppelaez <kppelaez@up.edu.ph>
2023-09-19 10:46:12 +0800

    Renamed SearchView to Search

--- PhoneBookCore/PhoneBookCore/Pages/Contacts/Search.cshtml.cs
+++ PhoneBookCore/PhoneBookCore/Pages/Contacts/Search.cshtml.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PhoneBookCore.Pages.Contacts
 {
-    public class SearchViewModel : PageModel
+    public class SearchModel : PageModel
     {
+        public string SearchString = string.Empty;
         public void OnGet()
         {
+            SearchString = "hi";
         }
     }
 }
