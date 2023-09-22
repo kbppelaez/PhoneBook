30fca4eccaf2bed55dbdc194fe4938c8d86fa615
kbppelaez <kppelaez@up.edu.ph>
2023-09-22 15:14:33 +0800

    Added the functions for GET/POST of Login

--- PhonebookV3/PhonebookV3/Controllers/UsersController.cs
+++ PhonebookV3/PhonebookV3/Controllers/UsersController.cs
@@ -1,14 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PhonebookV3.Models;
+using PhonebookV3.Core;
 
 namespace PhonebookV3.Controllers
 {
     public class UsersController : Controller
     {
-        [Route("/users/login", Name = "login")]
+        public readonly IUsersService _usersServices;
+        public readonly ILogger<UsersController> _logger;
+
+        public UsersController(IUsersService users, ILogger<UsersController> logger)
+        {
+            _usersServices = users;
+            _logger = logger;
+        }
+
+        [Route("/users/login")]
         [HttpGet]
         public IActionResult Login()
         {
-            return View();
+            return View(new UserViewModel());
+        }
+
+        [Route("/users/login")]
+        [HttpPost]
+        public async Task<IActionResult> VerifyLogin([FromForm] UserViewModel account)
+        {
+            await account.Verify();
+            if(account.success)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Login", account);
         }
 
 
