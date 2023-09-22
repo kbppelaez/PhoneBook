7d1c99cf236ee7f6300c0fcc0804fe2db3dbeb9b
kbppelaez <kppelaez@up.edu.ph>
2023-09-22 13:31:19 +0800

    Added controller for handling requests

---
+++ PhonebookV3/PhonebookV3/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhonebookV3.Controllers
+{
+    public class UsersController : Controller
+    {
+        [Route("/users/login", Name = "login")]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+    }
+}
