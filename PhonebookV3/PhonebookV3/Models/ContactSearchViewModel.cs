b9b30ca7b7adea1e73a975309fc3de455deaa839
kbppelaez <kppelaez@up.edu.ph>
2023-09-22 20:44:17 +0800

    -Added TotalPages variable and flags for next/back button; - created function to initialize said variables

--- PhonebookV3/PhonebookV3/Models/ContactSearchViewModel.cs
+++ PhonebookV3/PhonebookV3/Models/ContactSearchViewModel.cs
@@ -9,7 +9,9 @@ namespace PhonebookV3.Models
 
         public ContactListData[] Contacts { get; set; }
         public ContactSearchQueryData args { get; set; }
-
+        public int TotalPages { get; set; }
+        public bool HasPreviousPage { get; set; }
+        public bool HasNextPage { get; set; }
         public ContactSearchViewModel(IContactsService contactService)
         {
             _contactsService = contactService;
@@ -18,7 +20,17 @@ namespace PhonebookV3.Models
         public async Task Initialize(ContactSearchQueryData queryData)
         {
             args = queryData;
+            var count = await _contactsService.CountAll(args);
             Contacts = await _contactsService.GetAll(queryData);
+
+            InitializePageTracker(count);
+        }
+
+        public void InitializePageTracker(int count)
+        {
+            TotalPages = (int)Math.Ceiling(count/ (double) args.PageSize);
+            HasNextPage = (args.Page == TotalPages - 1) ? false : true;
+            HasPreviousPage = (args.Page == 0) ? false : true;
         }
     }
 }
