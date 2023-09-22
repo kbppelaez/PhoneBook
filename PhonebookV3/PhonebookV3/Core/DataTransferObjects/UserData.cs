ee81e22dcae33bf69ca3f22246ce959d6a899176
kbppelaez <kppelaez@up.edu.ph>
2023-09-22 15:11:25 +0800

    Created DTO and ViewModel for User

---
+++ PhonebookV3/PhonebookV3/Core/DataTransferObjects/UserData.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace PhonebookV3.Core.DataTransferObjects
+{
+    public class UserData
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(512)]
+        [EmailAddress(ErrorMessage = "Enter a valid email address.")]
+        [DisplayName("Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(64, MinimumLength = 8, ErrorMessage = "Password must have at least 8 characters.")]
+        [DataType(DataType.Password)]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+    }
+}
---
+++ PhonebookV3/PhonebookV3/Models/UserViewModel.cs
@@ -0,0 +1,35 @@
+﻿using PhonebookV3.Core.Application;
+using PhonebookV3.Core.DataTransferObjects;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhonebookV3.Models
+{
+    public class UserViewModel
+    {
+        // Constructors
+        public UserViewModel() { }
+
+        // Properties
+        public UsersService _usersService;
+        public UserData User {  get; set; }
+        public bool fromLogin { get; set; }
+        public bool success {  get; set; }
+        public string errorMsg { get; set; }
+
+        //Methods
+        public void AddService(UsersService usersService)
+        {
+            this._usersService = usersService;
+        }
+
+        public async Task Verify()
+        {
+            errorMsg = await _usersService.Verify(User);
+            if (errorMsg.Equals("OK"))
+                success = true;
+            else
+                success = false;
+        }
+    }
+}
