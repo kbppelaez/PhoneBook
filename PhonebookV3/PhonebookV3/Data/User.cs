07d434930e6151f7b2e4d1b67d1f441c8ca48bba
kbppelaez <kppelaez@up.edu.ph>
2023-09-22 13:30:14 +0800

    Created the User entity

---
+++ PhonebookV3/PhonebookV3/Data/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhonebookV3.Data
+{
+    public class User
+    {
+        /* Constructors */
+        public User() { }
+
+        /* Properties */
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(512)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string Password { get; set; }
+    }
+}
