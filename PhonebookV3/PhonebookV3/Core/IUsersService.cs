02600a0d2b261017f4b29ee89d8777f0712c5f35
kbppelaez <kppelaez@up.edu.ph>
2023-09-22 16:18:09 +0800

    Listed ViewModel-usable functions in the interface

--- PhonebookV3/PhonebookV3/Core/IUsersService.cs
+++ PhonebookV3/PhonebookV3/Core/IUsersService.cs
@@ -1,6 +1,13 @@
-﻿namespace PhonebookV3.Core
+﻿using PhonebookV3.Core.DataTransferObjects;
+using PhonebookV3.Data;
+
+namespace PhonebookV3.Core
 {
     public interface IUsersService
     {
+        Task<string> VerifyExisting(UserData account);
+        Task<User> Find(string Email);
+        Task<bool> VerifyNewAccount(string Email);
+        Task RegisterAccount(UserData newUser);
     }
 }
